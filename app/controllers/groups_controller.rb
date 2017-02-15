763332b7abd7f92be5b80990bc5446bf4d3c4ccb
daaaiki919 <tougedaiki@tougedaikinoMacBook-Pro.local>
2017-02-15 23:39:38 +0900

    redirection

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,16 @@
 class GroupsController < ApplicationController
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    redirect_to root_path
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
