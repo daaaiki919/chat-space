56c80c551b56cff36a614c08e059c38dae272cea
daaaiki919 <tougedaiki@tougedaikinoMacBook-Pro.local>
2017-02-22 01:15:18 +0900

    Create-group-index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
 
   def index
     @groups = current_user.groups
-    @group = Group.find_by(params[:group_id])
+    @group = Group.find(params[:group_id])
     @messages = @group.messages
     @users = @group.users
   end
