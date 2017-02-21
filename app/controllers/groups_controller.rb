f95120137d0646a3c9e5e71fe3b3c5927699ae5a
daaaiki919 <tougedaiki@tougedaikinoMacBook-Pro.local>
2017-02-21 13:15:50 +0900

    Edit-chatgroup

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,24 +1,31 @@
 class GroupsController < ApplicationController
 
+  def index
+    @groups = current_user.groups
+  end
+
   def new
     @group = Group.new
     @users = User.where.not(id: current_user)
   end
 
-  def edit
-    @group = Group.find(params[:id])
-  end
-
   def create
     @group = Group.new(group_params)
-     if current_user.name
-        @group.save
+      current_user.name
+     if @group.save
         redirect_to root_path, notice: '新しいグループを作成しました'
     else
       flash[:alert] = 'グループ作成に失敗しました'
       render new_group_path
     end
   end
+
+   def edit
+    @group = Group.includes(:users).find(params[:id])
+   end
+
+   def update
+   end
 
   private
 
