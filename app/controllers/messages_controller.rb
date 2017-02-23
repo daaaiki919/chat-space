497727a2ef56119dda384bd810039a4e56585375
daaaiki919 <tougedaiki@tougedaikinoMacBook-Pro.local>
2017-02-23 18:37:37 +0900

    message- Transmission

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,10 +5,23 @@ class MessagesController < ApplicationController
     @group = Group.find(params[:group_id])
     @messages = @group.messages
     @users = @group.users
+    @message = Message.new
   end
 
-  def self.time
-    created_at.strftime("%Y/%m/%d %H:%M:%S")
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+        redirect_to group_messages_path(params[:group_id])
+    else
+      render :index
+    end
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :groups, only: %i(index new edit create update) do
-    resources :messages, only: %i(index)
+    resources :messages, only: %i(index create)
   end
 end
