157c9e6fd4babfba027d8942cb2b714210a15125
daaaiki919 <tougedaiki@tougedaikinoMacBook-Pro.local>
2017-03-13 16:28:29 +0900

    message.js-update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,12 +1,16 @@
 $(function() {
   function buildHTML(message) {
-    var html = '<li class="message__contents">' +
+    var messageForm = '<li class="message__contents">' +
     '<p class="message__contents__sender">' +
     message.name +
     '<p class="message__contents__time">' +
-    message.time +
-    '<p class="message__contents__content">' +
-    message.body;
+    message.time;
+    if (message.image) {
+    var body = '<p class="message__contents__content">' + '<img src=' + message.image + '>';
+    } else {
+    var body = '<p class = "message__contents__content" >' + message.body;
+    }
+    var html = messageForm + body;
     return html;
   }
 
@@ -17,22 +21,23 @@ $(function() {
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var textField = $('.text');
-    var message_input = textField.val();
+    var fileField = $('.set_image');
+    var form = new FormData($(this).get(0));
     var message_url = $("#new_message").attr("action");
     $.ajax({
       type: 'POST',
       url: message_url,
-      data: {
-        message: {
-          body: message_input
-        }
-      },
-      dataType: 'json',
+      data: form,
+      processData: false,
+      contentType: false,
+      dataType: 'json'
+
     })
     .done(function(data) {
       var html = buildHTML(data);
       $('ul.message').append(html);
       textField.val('');
+      fileField.val('');
       scroll_view();
     })
     .fail(function() {
