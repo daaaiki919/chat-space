6a1779afabdd6c82b85c16cfbf6e2e8200724870
daaaiki919 <tougedaiki@tougedaikinoMacBook-Pro.local>
2017-03-21 16:01:03 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
-  path: "~/.rbenv/shims:~/.rbenv/bin:$PATH",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   region: ENV['AWS_REGION'],
   aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
   aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
