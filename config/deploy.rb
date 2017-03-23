9a66a6bf952899da0b026fe3e3b1bc1ffb084ae9
daaaiki919 <tougedaiki@tougedaikinoMacBook-Pro.local>
2017-03-23 11:40:32 +0900

    delete

--- config/deploy.rb
+++
@@ -1,7 +0,0 @@
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_REGION: ENV['AWS_REGION'],
-  AWS_ACCESS_KEY_ID: ENV['AWS_ACCESS_KEY_ID'],
-  AWS_SECRET_ACCESS_KEY: ENV['AWS_SECRET_ACCESS_KEY']
-}
